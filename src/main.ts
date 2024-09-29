dd940157c800bf6e31ab46a4e523c9782a2130bf
Vitor Melo <20214218-VitorMelo15@users.noreply.replit.com>
2024-09-29 02:42:41 +0000

    added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infrastructure/http/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import {AllExceptionsFilter} from './infrastructure/exceptions/all-exceptions.filter';
+import { AllExceptionsFilter } from './infrastructure/exceptions/all-exceptions.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(helmet());
+  //TODO add CORS
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
 
