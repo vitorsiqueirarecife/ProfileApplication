8fce1d5e327b66fb6fd05d2f723bee7f908133a1
vitorsiqueirarecife <vitormelosiqueira@gmail.com>
2024-10-19 15:44:54 -0300

    handle errors

--- mic-auth/src/application/services/user.service.ts
+++ mic-auth/src/application/services/user.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  UnauthorizedException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { IUser } from 'src/domain/interfaces/user/user.interface';
 import {
   IUserRepository,
@@ -12,7 +17,18 @@ export class UserService implements IUserService {
   constructor(
     @Inject(USER_REPOSITORY) private userRepository: IUserRepository,
   ) {}
+
   async findByEmail(email: string): Promise<IUser> {
-    return await this.userRepository.findByEmail(email);
+    try {
+      const foundUser = await this.userRepository.findByEmail(email);
+
+      if (!foundUser) {
+        throw new UnauthorizedException('User not found');
+      }
+
+      return foundUser;
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 }
